28ffb43e4594abe59f62a0309df742dff0decd38
Ditharles <ditharles.agbehounkpan@gmail.com>
2024-12-01 17:52:31 +0100

    end
    It is necessary to improve flask page comportement and input logic

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from pyspark.sql import SparkSession
 from pyspark.ml.linalg import Vectors
-from pyspark.ml.feature import VectorAssembler,StringIndexer
+from pyspark.ml.feature import VectorAssembler, StringIndexer
 from pyspark.ml.regression import RandomForestRegressionModel
 
 
@@ -21,37 +21,37 @@ def index():
         # Récupérer les données du formulaire
         Airlines = request.form.get("airlines", "")
         Total_Stops = int(request.form["stops"])
-        Destination = float(request.form["destination"])
+        Destination = request.form.get("destination", "")
         Duration = float(request.form["duration"])
         Date = request.form.get("date", "")
    
-
         # Créer un DataFrame Spark avec les données saisies
         data = [(Airlines, Total_Stops, Duration, Destination, Date)] 
         columns = ["Airlines", "Total_Stops", "Duration", "Destination", "Date"]  
         df = spark.createDataFrame(data, columns)
 
+        # Appliquer StringIndexer sur les colonnes catégorielles
+        indexers = [StringIndexer(inputCol=column, outputCol=column + "_index").fit(df)
+                    for column in ["Airlines", "Destination", "Date"]]  # inclure "Date" si besoin
         
-        indexers = [StringIndexer(inputCol=column,   outputCol=column + "_index").fit(df) 
-            for column in ["Airlines","Date", "Destination"]]
-
+        # Appliquer les transformations d'indexation
         for indexer in indexers:
-            new_data = indexer.transform(new_data)
+            df = indexer.transform(df)  # Transformez df avec l'indexeur
 
-        assembler = VectorAssembler(inputCols=["Airlines_index", "Total_Stops", "Duration", "Destination_index","Date_index"], outputCol="features")
-        new_data = assembler.transform(new_data)
-        new_data = new_data.select("features")
+        # Assembler les features dans une seule colonne "features"
+        assembler = VectorAssembler(inputCols=["Airlines_index", "Total_Stops", "Duration", "Destination_index", "Date_index"], outputCol="features")
+        df = assembler.transform(df)
 
+        # Prédiction
+        prediction = model.transform(df)
 
-        
-        prediction = model.transform(new_data)
-
-        
+        # Récupérer la prédiction (la première valeur de "prediction")
         result = prediction.select("prediction").collect()[0]["prediction"]
         
-        return render_template("index.html", result=result)
+        return render_template("index.html", result=int(result))
 
     return render_template("index.html", result=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
